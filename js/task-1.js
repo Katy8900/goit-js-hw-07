d98f67dad763f4c2cbb7a22d209b6073a29b1142
Katy8900 <katerynamatchak@gmail.com>
2025-06-22 22:57:27 +0200

    HW JS7

--- js/task-1.js
+++ js/task-1.js
@@ -1,33 +1,11 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
+const categoriesListRef = document.querySelector('#categories');
+const categoryItems = categoriesListRef.querySelectorAll('.item');
 
-  getBalance() {
-    return this.balance;
-  },
+console.log(`Number of categories: ${categoryItems.length}`);
 
-  getDiscount() {
-    return this.discount;
-  },
-
-  setDiscount(value) {
-    this.discount = value;
-  },
-
-  getOrders() {
-    return this.orders;
-  },
-
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-};
-
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+categoryItems.forEach(item => {
+  const title = item.querySelector('h2').textContent;
+  const elementsCount = item.querySelectorAll('li').length;
+  console.log(`Category: ${title}`);
+  console.log(`Elements: ${elementsCount}`);
+});
---
+++ js/task-4.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const emailInput = form.elements.email;
+  const passwordInput = form.elements.password;
+
+  const email = emailInput.value.trim();
+  const password = passwordInput.value.trim();
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const userData = {
+    email: email,
+    password: password,
+  };
+
+  console.log(userData);
+
+  form.reset();
+});
